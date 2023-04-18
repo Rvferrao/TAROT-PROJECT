939d569c878b4698335eb40934b5dcca8e1f3936
Ricardo Veronezi Ferrão <rvferrao@gmail.com>
2023-04-18 08:54:25 -0300

    feat: cards are now shuffled

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import { useEffect, useState } from "react";
 import { Card } from "./components/Card";
-import axios from "axios";
 import { Button } from "./components/Button";
+import { shuffledCards } from "./helpers/ShuffledCards";
+import axios from "axios";
 
 export const App = () => {
   const [card, setCard] = useState();
   const [shown, setShown] = useState(false);
+  const [gameStarted, setGameStarted] = useState(false);
 
   useEffect(() => {
     handleRequest();
   }, []);
+
+  useEffect(() => {
+    if (card) {
+      handleStartGame(card);
+    }
+  }, [gameStarted]);
 
   const handleRequest = async () => {
     await axios
@@ -31,16 +39,23 @@ export const App = () => {
       });
   };
 
-  const handleControlCard = () => {
+  const handleControlGame = () => {
     setShown(!shown);
+    setGameStarted(!gameStarted);
+  };
+
+  const handleStartGame = (card) => {
+    const initialCards = [...card.cards];
+    const newCards = shuffledCards(initialCards);
+    setCard({ ...card, cards: newCards });
   };
 
   return (
     <div className="App">
-      <div className="flex flex-col justify-center items-center font-bold text-[38px]">
-        <h1>TAROT GAME</h1>
+      <div className="flex flex-col justify-center items-center font-bold text-[38px] pt-6">
+        <h1 className="font-bold">TAROT GAME</h1>
         <div>
-          <Button label={shown ? "Pause" : "Start"} value={handleControlCard} />
+          <Button label={shown ? "Pause" : "Start"} value={handleControlGame} />
         </div>
       </div>
       <div className="flex justify-center items-center p-4">
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,7 +2,7 @@ export const Card = ({ card, shown }) => {
   return (
     <div>
       {shown ? (
-        <div className="flex justify-center items-center transition-all duration-1000 transform-origin-right -rotate-180  w-[160px] h-[300px] my-2 ">
+        <div className="flex justify-center items-center transition-all duration-1000  -rotate-180  w-[160px] h-[300px] my-2 cursor-pointer">
           <img
             className="w-[100%] h-[100%]"
             src={card.imageBackCard}
@@ -10,7 +10,7 @@ export const Card = ({ card, shown }) => {
           />
         </div>
       ) : (
-        <div className="flex justify-center items-center shadow-lg bg-blue-500 w-[160px] h-[300px] my-2  rounded-lg">
+        <div className="flex justify-center items-center shadow-lg bg-blue-500 w-[160px] h-[300px] my-2  rounded-lg cursor-pointer">
           <img
             className="w-[140px] h-[280px]"
             src={card.imageFront}

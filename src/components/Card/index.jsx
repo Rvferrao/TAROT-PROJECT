5e8be668dc4fcbe5d61fd04ad559092849b7b5ae
Ricardo Veronezi Ferrão <rvferrao@gmail.com>
2023-05-09 22:42:42 -0300

    fix: cards are now functional

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,20 +1,48 @@
-export const Card = ({ card, shown }) => {
+import { CardContent } from "../CardContent";
+
+export const Card = ({
+  card,
+  shown,
+  showContent,
+  upCard,
+  index,
+  selectedIndex,
+  setSelectedIndex,
+  handleShowCard,
+  isClicked,
+  setIsClicked,
+}) => {
   return (
     <div>
       {shown ? (
-        <div className="flex justify-center items-center transition-all duration-1000 rounded-sm  -rotate-180  w-[160px] h-[300px] my-2 cursor-pointer">
-          <img
-            className="w-[100%] h-[100%]"
-            src={card.imageBackCard}
-            alt={"card " + card.name.toLowerCase()}
-          />
+        <div
+          className="flex justify-center items-center transition-all duration-[3000ms] rounded-sm -rotate-[360deg] w-[170px] h-[320px] my-2 cursor-pointer"
+          onClick={() => {
+            setIsClicked(true);
+            if (!isClicked) {
+              handleShowCard(card.name);
+              setSelectedIndex(index);
+            }
+          }}
+        >
+          {index === selectedIndex && isClicked ? (
+            <div className="flex justify-center items-center shadow-lg bg-blue-500 w-[170px] h-[320px] my-2 rounded-lg cursor-pointer">
+              <CardContent upCard={upCard} showContent={showContent} />
+            </div>
+          ) : (
+            <img
+              className="w-[100%] h-[100%]"
+              src={card?.imageBackCard}
+              alt={"card " + card?.name.toLowerCase()}
+            />
+          )}
         </div>
       ) : (
-        <div className="flex justify-center items-center shadow-lg bg-blue-500 w-[160px] h-[300px] my-2  rounded-lg cursor-pointer">
+        <div className="flex justify-center items-center shadow-lg bg-blue-500 w-[170px] h-[320px] my-2 rounded-lg cursor-pointer">
           <img
             className="w-[140px] h-[280px]"
-            src={card.imageFront}
-            alt={"carta: " + card.name.toLowerCase()}
+            src={card?.imageFront}
+            alt={"carta: " + card?.name.toLowerCase()}
           />
         </div>
       )}
